fd3ac14f7caa02d39becbe13beb78d013400ed80
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-10 22:47:13 +0200

    add useState and type arrey tracks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-const tracks = [
-  {
-    title: "Musicfun soundtrack",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-  {
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-  {
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-  {
-    title: "Musicfun soundtrack instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-]
+import { useState } from "react"
+
+type Track = {
+  id: number
+  title: string
+  url: string
+}
  
 export function App() {
+  const [tracks] = useState <Track[]>([
+  {
+      id: 1,
+      title: "Musicfun soundtrack",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+      id: 2,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+      id: 3,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+      id: 4,
+      title: "Musicfun soundtrack instrumental",
+      url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+])
+
   return (
     <>
       <h1>Musicfun Player</h1>
