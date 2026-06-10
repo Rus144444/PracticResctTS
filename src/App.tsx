15db9efac8c237cc929d4a5ae30e652a45f1d363
Ruslan Azizov <Maks@maks-macbook-air.home>
2026-06-10 23:09:38 +0200

    app component add useEffect and test logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 type Track = {
   id: number
@@ -7,8 +7,11 @@ type Track = {
 }
  
 export function App() {
-  const [tracks] = useState<Track[] | null>([
-  {
+  const [tracks, setTracks] = useState<Track[] | null>(null)
+
+  useEffect(() => {
+    setTimeout(()=> {
+    setTracks([{
       id: 1,
       title: "Musicfun soundtrack",
       url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
@@ -28,20 +31,22 @@ export function App() {
       title: "Musicfun soundtrack instrumental",
       url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
     },
-])
+    ])
+  }, 3000)
+  },[])
 
   return (
     <>
       <h1>Musicfun Player</h1>
-      {tracks === null && <span>Loading...</span>}
-      {tracks?.length === 0 && <span>No tracks</span>}
+      {tracks === null && <div>Loading...</div>}
+      {tracks?.length === 0 && <div>No tracks</div>}
       <ul>
         {tracks?.map((track) => {
           return (
-            <li>
+            <li key={track.id}>
               <div>{track.title}</div>
               <audio
-                controls //controls={true}
+                controls
                 src={track.url}
               ></audio>
             </li>
